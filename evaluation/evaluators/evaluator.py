26d64b3ee94b3d96ce66def9e934b70538b655b7
Lukas Abegg <lukas.abegg1@gmail.com>
2020-09-15 14:36:31 +0200

    implement evaluation

--- evaluation/evaluators/evaluator.py
+++ evaluation/evaluators/evaluator.py
@@ -50,7 +50,7 @@ class Evaluator:
                 if self.experiment is not None:
                     self.experiment.log_metric("bleu_score", sentence_bleu_score)
                     self.experiment.log_metric("meteor_score", meteor_score)
-                    self.experiment.log_others("input_output:", str(round+":"+origin+"_"+hypothesis))
+                    self.experiment.log_others("input_output:", str(round)+":"+origin+"_"+hypothesis)
 
     def _tokenize(self, x):
         tokenized = self.predictor.tokenizer.tokenize(x)
