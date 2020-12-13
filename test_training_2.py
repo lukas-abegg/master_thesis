33ed81a32300d3d6389f28ec56d5f1260c0d3cd3
Lukas Abegg <lukas.abegg1@gmail.com>
2020-12-13 23:23:44 +0100

    Test something else

--- test_training_2.py
+++ test_training_2.py
@@ -317,7 +317,7 @@ if __name__ == "__main__":
         "d_model": 512,
         "n_head": 8,
         "dim_feedforward": 2048,
-        "n_layers": 2,
+        "n_layers": 6,
         "dropout": 0.1,
         "load_embedding_weights": False
     }
