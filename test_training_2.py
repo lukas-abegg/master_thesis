305a93f096fc66e816655ddc048cf975c976b90a
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-03 02:10:44 +0100

    Test something else

--- test_training_2.py
+++ test_training_2.py
@@ -319,9 +319,9 @@ if __name__ == "__main__":
     print("Use device ", device, " for task")
 
     hyper_params = {
-        "dataset": "wikilarge",  # mws # iwslt #pwkp #wikilarge
-        "sequence_length_src": 60,
-        "sequence_length_tgt": 50,
+        "dataset": "newsela",  # mws # iwslt #pwkp #wikilarge
+        "sequence_length_src": 55,
+        "sequence_length_tgt": 35,
         "batch_size": 50,
         "num_epochs": 100,
         "learning_rate": 1e-4,
@@ -334,10 +334,10 @@ if __name__ == "__main__":
     }
 
     bert_path = "/glusterfs/dfs-gfs-dist/abeggluk/zzz_bert_models_1/bert_base_cased_12"
-    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/wikilarge_transformer/_0"
-    project_name = "transformer-wikilarge"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0_2"
+    project_name = "transformer-newsela"
     tracking_active = True
-    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_4"
+    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_5"
 
     max_len_src = hyper_params["sequence_length_src"]
     max_len_tgt = hyper_params["sequence_length_tgt"]
