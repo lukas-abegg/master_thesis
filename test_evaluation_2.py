1e350b3bb0ee630371f2bb4b335c40112aec1c55
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-06 00:25:34 +0100

    Test something else

--- test_evaluation_2.py
+++ test_evaluation_2.py
@@ -155,7 +155,7 @@ def validate(origin_groups, reference_groups, prediction_groups, experiment=None
 if __name__ == "__main__":
     project_name = "transformer-mws-eval"  # newsela-transformer-bert-weights
     tracking_active = True
-    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_2/evaluation/mle"
+    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_3/evaluation/mle"
 
     experiment = None
     if tracking_active:
--- test_predicting_2.py
+++ test_predicting_2.py
@@ -148,15 +148,15 @@ if __name__ == "__main__":
         "d_model": 512,
         "n_head": 8,
         "dim_feedforward": 2048,
-        "n_layers": 6,
+        "n_layers": 4,
         "dropout": 0.1,
         "load_embedding_weights": False
     }
 
     bert_path = "/glusterfs/dfs-gfs-dist/abeggluk/zzz_bert_models_1/bert_base_cased_12"
-    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_2/checkpoints/mle"
-    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_2/evaluation/mle"
-    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_1"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_3/checkpoints/mle"
+    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_0_3/evaluation/mle"
+    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_2"
 
     max_len_src = hyper_params["sequence_length_src"]
     max_len_tgt = hyper_params["sequence_length_tgt"]
