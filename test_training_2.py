f831f2087dc07ad9cd58933ee7ed44c5abe8c478
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-05 23:58:17 +0100

    Test something else

--- test_training_2.py
+++ test_training_2.py
@@ -331,13 +331,13 @@ if __name__ == "__main__":
         "d_model": 512,
         "n_head": 8,
         "dim_feedforward": 2048,
-        "n_layers": 8,
+        "n_layers": 4,
         "dropout": 0.1,
         "load_embedding_weights": False
     }
 
     bert_path = "/glusterfs/dfs-gfs-dist/abeggluk/zzz_bert_models_1/bert_base_cased_12"
-    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_5"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_4"
     project_name = "transformer-newsela"
     tracking_active = True
     base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_7"
