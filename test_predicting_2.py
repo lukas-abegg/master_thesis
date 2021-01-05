ef5c7cc0cb2e87063e4c29e370b22a7313f423e7
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-05 18:38:33 +0100

    Test something else

--- test_predicting_2.py
+++ test_predicting_2.py
@@ -141,8 +141,8 @@ if __name__ == "__main__":
     hyper_params = {
         "dataset": "newsela",  # mws
         "tokenizer": "word",  # wordpiece
-        "sequence_length_src": 72,
-        "sequence_length_tgt": 43,
+        "sequence_length_src": 54,
+        "sequence_length_tgt": 32,
         "batch_size": 50,
         "learning_rate": 1e-4,
         "d_model": 512,
@@ -154,9 +154,8 @@ if __name__ == "__main__":
     }
 
     bert_path = "/glusterfs/dfs-gfs-dist/abeggluk/zzz_bert_models_1/bert_base_cased_12"
-    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/checkpoints/mle"
-    #checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer_bert_weights/checkpoints/mle"
-    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/evaluation/mle"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0/checkpoints/mle"
+    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0/evaluation/mle"
     base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_1"
 
     max_len_src = hyper_params["sequence_length_src"]
