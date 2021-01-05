cb263f0f7f5bf561a8d47050a2b520dc3c099a0b
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-05 19:00:25 +0100

    Test something else

--- test_predicting_2.py
+++ test_predicting_2.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     }
 
     bert_path = "/glusterfs/dfs-gfs-dist/abeggluk/zzz_bert_models_1/bert_base_cased_12"
-    checkpoint_base = "	/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0_2/checkpoints/mle"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0_2/checkpoints/mle"
     save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_0_2/evaluation/mle"
     base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_1"
 
