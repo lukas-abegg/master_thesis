b87e2d2c20d903513301dfc93cd12772c2c53f3f
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-08 18:26:51 +0100

    Test something else

--- test_training_2_BART.py
+++ test_training_2_BART.py
@@ -380,16 +380,16 @@ if __name__ == "__main__":
         "batch_size": 15,
         "num_epochs": 10,
         "learning_rate": 1e-5,
-        "bart_model": "facebook/bart-large"  # facebook/bart-large-cnn
+        "bart_model": "facebook/bart-large-cnn"  # facebook/bart-large-cnn
     }
 
     tokenizer = BartTokenizer.from_pretrained(hyper_params["bart_model"])
     model = BartForConditionalGeneration.from_pretrained(hyper_params["bart_model"])
 
-    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_bart/_0"
+    checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_bart/_1"
     project_name = "bart-newsela"
     tracking_active = True
-    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_8"
+    base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_7"
 
     max_len_src = hyper_params["sequence_length_src"]
     max_len_tgt = hyper_params["sequence_length_tgt"]
