f3bdfa89790d8606779cec9878668df64c543c6d
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-11 15:54:49 +0100

    Test something else

--- test_predicting_2_BART.py
+++ test_predicting_2_BART.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
         "sequence_length_src": 76,
         "sequence_length_tgt": 65,
         "batch_size": 15,
-        "beam_size": 1,
+        "beam_size": 4,
         "bart_model": "facebook/bart-large-cnn"  # facebook/bart-large-cnn
     }
 
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     model = BartForConditionalGeneration.from_pretrained(hyper_params["bart_model"])
 
     checkpoint_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_bart/_1/checkpoints/mle"
-    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_bart/_1/evaluation/mle"
+    save_run_files_base = "/glusterfs/dfs-gfs-dist/abeggluk/mws_bart/_1/evaluation/mle/_4"
     base_path = "/glusterfs/dfs-gfs-dist/abeggluk/data_1"
 
     max_len_src = hyper_params["sequence_length_src"]
