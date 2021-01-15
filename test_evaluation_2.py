2d6ccebf16325032d3b608cc2a8bebf963e5eeea
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-15 17:07:00 +0100

    Test something else

--- test_evaluation_2.py
+++ test_evaluation_2.py
@@ -198,7 +198,7 @@ def validate(origin_groups, reference_groups, prediction_groups, experiment=None
 if __name__ == "__main__":
     project_name = "gan-newsela-eval"  # newsela-transformer-bert-weights
     tracking_active = True
-    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6/evaluation/joint/pgloss_sari/_5/"
+    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6/evaluation/joint/pgloss_sari/_best/"
 
     experiment = None
     if tracking_active:
