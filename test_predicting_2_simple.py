23a213376823ef53087c8f613032fb9275b5f197
Lukas Abegg <lukas.abegg1@gmail.com>
2021-01-25 01:22:27 +0100

    Test something else

--- test_predicting_2_simple.py
+++ test_predicting_2_simple.py
@@ -368,18 +368,20 @@ if __name__ == "__main__":
     ]
 
     experiments_newsela_wordpiece = [
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/mle/beam",
-         "model": "checkpoints/mle/best_model.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/reward/beam_1",
-         "model": "checkpoints/joint/reward/best_generator_g_model.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6_1", "eval": "evaluation/joint/reward/beam_2",
-         "model": "checkpoints/joint/reward/joint_2.769.epoch_4.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6_1", "eval": "evaluation/joint/pgloss/beam",
-         "model": "checkpoints/joint/pgloss/best_generator_g_model.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/pgloss_sari/beam_2",
-         "model": "checkpoints/joint/pgloss_sari/joint_2.803.epoch_5.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/pgloss_sari/beam_1",
-         "model": "checkpoints/joint/pgloss_sari/best_generator_g_model.pt", "n_layers": 4}
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/mle/beam",
+        #  "model": "checkpoints/mle/best_model.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/reward/beam_1",
+        #  "model": "checkpoints/joint/reward/best_generator_g_model.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6_1", "eval": "evaluation/joint/reward/beam_2",
+        #  "model": "checkpoints/joint/reward/joint_2.769.epoch_4.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6_1", "eval": "evaluation/joint/pgloss/beam",
+        #  "model": "checkpoints/joint/pgloss/best_generator_g_model.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/pgloss_sari/beam_2",
+        #  "model": "checkpoints/joint/pgloss_sari/joint_2.803.epoch_5.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_6", "eval": "evaluation/joint/pgloss_sari/beam_1",
+        #  "model": "checkpoints/joint/pgloss_sari/best_generator_g_model.pt", "n_layers": 4},
+        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/newsela_transformer/_7", "eval": "evaluation/mle/beam",
+         "model": "checkpoints/mle/best_model.pt", "n_layers": 4}
     ]
 
     experiments_mws_word = [
@@ -390,16 +392,18 @@ if __name__ == "__main__":
     ]
 
     experiments_mws_wordpiece = [
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_2", "eval": "evaluation/mle/beam",
-         "model": "checkpoints/mle/best_model.pt", "n_layers": 6},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/mle/beam",
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_2", "eval": "evaluation/mle/beam",
+        #  "model": "checkpoints/mle/best_model.pt", "n_layers": 6},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/mle/beam",
+        #  "model": "checkpoints/mle/best_model.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/joint/pgloss/beam_1",
+        #  "model": "checkpoints/joint/pgloss/joint_1.144.epoch_1.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3_1", "eval": "evaluation/joint/pgloss/beam_2",
+        #  "model": "checkpoints/joint/pgloss/best_generator_g_model.pt", "n_layers": 4},
+        # {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/joint/reward/beam",
+        #  "model": "checkpoints/joint/reward/best_generator_g_model.pt", "n_layers": 4},
+        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_7", "eval": "evaluation/mle/beam",
          "model": "checkpoints/mle/best_model.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/joint/pgloss/beam_1",
-         "model": "checkpoints/joint/pgloss/joint_1.144.epoch_1.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3_1", "eval": "evaluation/joint/pgloss/beam_2",
-         "model": "checkpoints/joint/pgloss/best_generator_g_model.pt", "n_layers": 4},
-        {"base_path": "/glusterfs/dfs-gfs-dist/abeggluk/mws_transformer/_2_3", "eval": "evaluation/joint/reward/beam",
-         "model": "checkpoints/joint/reward/best_generator_g_model.pt", "n_layers": 4}
     ]
 
     experiments_pwkp_wordpiece = [
@@ -411,10 +415,10 @@ if __name__ == "__main__":
 
     experiments = [
         # {"transformer_model": "newsela_word", "dataset": "newsela", "tokenizer": "word", "experiments": experiments_newsela_word},
-        # {"transformer_model": "newsela_wordpiece", "dataset": "newsela", "tokenizer": "wordpiece", "experiments": experiments_newsela_wordpiece},
+        {"transformer_model": "newsela_wordpiece", "dataset": "newsela", "tokenizer": "wordpiece", "experiments": experiments_newsela_wordpiece},
         # {"transformer_model": "mws_word", "dataset": "mws", "tokenizer": "word", "experiments": experiments_mws_word},
-        # {"transformer_model": "mws_wordpiece", "dataset": "mws", "tokenizer": "wordpiece", "experiments": experiments_mws_wordpiece}
-        {"transformer_model": "pwkp_wordpiece", "dataset": "pwkp", "tokenizer": "wordpiece", "experiments": experiments_pwkp_wordpiece}
+        {"transformer_model": "mws_wordpiece", "dataset": "mws", "tokenizer": "wordpiece", "experiments": experiments_mws_wordpiece}
+        # {"transformer_model": "pwkp_wordpiece", "dataset": "pwkp", "tokenizer": "wordpiece", "experiments": experiments_pwkp_wordpiece}
     ]
 
     for set in experiments:
