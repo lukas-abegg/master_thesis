baa7a7bed1dde21c6fea64bf3a9ba9e5cb173f0d
Lukas Abegg <lukas.abegg1@gmail.com>
2021-02-02 00:16:21 +0100

    Test something else

--- test_evaluation_2_simple.py
+++ test_evaluation_2_simple.py
@@ -238,9 +238,9 @@ def validate(origin_groups, reference_groups, prediction_groups, beam_sizes, exp
 
 
 if __name__ == "__main__":
-    project_name = "transformer-mws-eval-beam"  # newsela-transformer-bert-weights
+    project_name = "transformer-pwkp-eval-beam"  # newsela-transformer-bert-weights
     tracking_active = True
-    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/edit_nts/evaluations/newsela/"
+    base_file_path = "/glusterfs/dfs-gfs-dist/abeggluk/edit_nts/evaluations/pwkp/"
 
     experiment = None
     if tracking_active:
